a386b6c35898cdecc79943b2807da29f2f584892
Ariva2 <ariva080503@gmail.com>
2023-05-13 21:20:22 +0700

    1.5 INNERITANCE

---
+++ src/main/java/Model/MahasiswaTransfer.java
@@ -0,0 +1,12 @@
+package Model;
+public class MahasiswaTransfer extends Mahasiswa{
+    private double IPKSebelumnya;
+    
+    public double getIPKSembelumnya() {
+        return IPKSebelumnya;
+    }
+    
+    public void setIPKSebelumnya (double IPKSebelumnya) {
+        this.IPKSebelumnya = IPKSebelumnya;
+    }
+}
--- src/main/java/com/mycompany/praktikum01/Praktikum01.java
+++ src/main/java/com/mycompany/praktikum01/Praktikum01.java
@@ -1,6 +1,7 @@
 package com.mycompany.praktikum01;
 import Model.Mahasiswa;
 import Model.Santri;
+import Model.MahasiswaTransfer;
 public class Praktikum01 {
     public static void main(String[] args) {
         Mahasiswa mahasiswa = new Mahasiswa();
@@ -14,6 +15,14 @@ public class Praktikum01 {
                 211002,
                 "MUSLIM",
                 15.22);
+        
+        MahasiswaTransfer mt = new MahasiswaTransfer();
+        mt.setId(3);
+        mtsetNPM("17990003")
+        mt.setNama("Utaman Ibn Affan");
+        mt.setIPK(3.9);
+        mt.viewData();
+
         
         mahasiswa.setId(1);
         mahasiswa.setNPM("2110010316");
